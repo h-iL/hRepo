fa548d8e08ea89d6afc82103dc20fc148d50f9d8
h-iL <80236301+h-iL@users.noreply.github.com>
2021-03-10 16:07:51 +0800

    push

--- vr-test-scn/public/sketch.js
+++ vr-test-scn/public/sketch.js
@@ -1,39 +1,52 @@
  import {VRButton} from './js/VRButton.js'
 
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var scene = new THREE.Scene()
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-camera.position.set(0, 0, 4);
+camera.position.set(0, 0, 0)
 
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+var renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
 
-renderer.xr.enabled = true;
+renderer.xr.enabled = true
 
-document.body.appendChild(renderer.domElement);
-document.body.appendChild(VRButton.createButton(renderer));
+document.body.appendChild(renderer.domElement)
+document.body.appendChild(VRButton.createButton(renderer))
 
 
 //detect window size change and update projectionmatrix to current width/height
 window.addEventListener('resize', function () {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
+    var width = window.innerWidth
+    var height = window.innerHeight
+    renderer.setSize(width, height)
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
 });
 
 //*********************************************************//
 
 
-var geometry = new THREE.BoxGeometry(6, 2, 2);
-var material = new THREE.MeshBasicMaterial({ color: 'red', side: THREE.DoubleSide });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var roomGeometry = new THREE.BoxGeometry(5, 3, 5)
+var material = new THREE.MeshBasicMaterial({ color: 'blue', side: THREE.DoubleSide })
 
-var ambientLight = new THREE.AmbientLight('white', 3);
-scene.add(ambientLight);
+var roomMaterials =
+    [
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./img/01.jpeg'), side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./img/02.jpeg'), side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({color:'black', side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({color:'black', side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./img/03.jpeg'), side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./img/04.jpeg'), side: THREE.DoubleSide })
+    ]
+
+
+var roomCube = new THREE.Mesh(roomGeometry, roomMaterials)
+scene.add(roomCube)
+roomCube.position.set(0, 1, 0)
+
+var ambientLight = new THREE.AmbientLight('white', 3)
+scene.add(ambientLight)
 
 
 
@@ -44,23 +57,15 @@ scene.add(ambientLight);
 var update = function ()
 {
 
-};
+}
 
-var render = function ()
-{
-    renderer.render(scene, camera)
-};
+//var render = function ()
+//{
+//    renderer.render(scene, camera)
+//}
 
-var GameLoop = function ()
-{
-    requestAnimationFrame(GameLoop);
-    update();
-    render();
-};
-
-//GameLoop();
 
 renderer.setAnimationLoop(function ()
 {
     renderer.render(scene, camera)
-});
+})
